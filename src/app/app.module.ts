aaacb9374dbbb8dc67b78172e82e8756dae18cc0
Tom <vaibhav@ubuntu.ubuntu-domain>
2018-03-10 13:22:16 +0530

    final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,21 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { AngularFireModule } from "angularfire2";
+import { AngularFireAuthModule } from "angularfire2/auth";
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+
+
+var config = {
+  apiKey: "AIzaSyA5OVs9IT1mpvIIqeVNrozixjcettFVf0I",
+  authDomain: "sociallogins-fbe89.firebaseapp.com",
+  databaseURL: "https://sociallogins-fbe89.firebaseio.com",
+  projectId: "sociallogins-fbe89",
+  storageBucket: "sociallogins-fbe89.appspot.com",
+  messagingSenderId: "637140507181"
+};
 
 @NgModule({
   declarations: [
@@ -14,7 +26,9 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(config),
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,9 @@
+
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AngularFireAuth } from 'angularfire2/auth';
+import firebase from "firebase";
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +11,41 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  provider = {
+    loggedIn: false,
+    name: '',
+    profilePicture: '',
+    email: ''
+  }
+
+  login(provider) {
+    let signInProvider = null;
+    switch(provider) {
+      case "facebook":
+        signInProvider = new firebase.auth.FacebookAuthProvider();
+        break;
+      case "google":
+        signInProvider = new firebase.auth.GoogleAuthProvider();
+        break;
+    }
+
+    this.afa.auth.signInWithPopup(signInProvider)
+    .then((res) => {
+        this.provider.loggedIn = true;
+        this.provider.name = res.user.displayName;
+        this.provider.email = res.user.email;
+        this.provider.profilePicture = res.user.photoURL;
+    });
+  }
+
+  constructor(
+    public navCtrl: NavController,
+    private afa: AngularFireAuth) {
 
   }
 
+  logout() {
+    this.afa.auth.signOut();
+    this.provider.loggedIn = false;
+  }
 }
